7ac522f9dc85a1a437362626a6052b505e502980
PaulDenis <d.paul@outlook.it>
2021-05-04 15:15:14 +0200

    Snack#5 completo

---
+++ Snack#5/script/script.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto.
+
+var numeriDispari = [];
+// Chiedi per 6 volte all’utente di inserire un numero
+for (var i = 0; i < 6; i++ ) {
+    var num = parseInt(prompt("Inserisci un numero"));
+    // console.log(num);
+    if(isEven(num)) {
+        numeriDispari.push(num);
+        // console.log(numeriDispari);
+    }
+}
+console.log(numeriDispari);
+
+// se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
+// Stampa l'array nella console.

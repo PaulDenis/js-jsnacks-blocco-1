2eba5c89e6ba30c51d41a75e2fb5afda88ee9b36
PaulDenis <d.paul@outlook.it>
2021-05-04 17:43:34 +0200

    Aggiunti commenti

--- Snack#5/script/functions.js
+++ Snack#5/script/functions.js
@@ -1,3 +1,3 @@
 function isOdd(num) {
-    return num % 2 == 1;
+    return num % 2 == 1; //Se il numero è dispari, return = true
 }
--- Snack#5/script/script.js
+++ Snack#5/script/script.js
@@ -3,14 +3,14 @@
 var numeriDispari = [];
 // Chiedi per 6 volte all’utente di inserire un numero
 for (var i = 0; i < 6; i++ ) {
+
+    // se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
     var num = parseInt(prompt("Inserisci un numero"));
     // console.log(num);
-    if(isOdd(num)) {
+    if(isOdd(num)) { //se isOdd = true, allora entra nel ciclo
         numeriDispari.push(num);
-        // console.log(numeriDispari);
     }
 }
-console.log(numeriDispari);
 
-// se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
 // Stampa l'array nella console.
+console.log(numeriDispari);
